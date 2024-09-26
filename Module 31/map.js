f541023f1cac2b3ae9d7d308362f9eac0b30affb
adnanahid <sikuahamed@gmail.com>
2024-09-26 22:31:56 +0600

    don3

---
+++ Module 31/map.js
@@ -0,0 +1,28 @@
+const numbers = [4, 5, 6, 7, 8, 9];
+const find = numbers.find(n => n>9)
+console.log(find);
+const filter = numbers.filter(n => n>9)
+console.log(filter);
+// const doubled = [];
+// for (const num of numbers) {
+//   const double = num * 2;
+//   doubled.push(double);
+// }
+// console.log(doubled);
+
+
+
+// function doubleIt(num) {
+//   return num * 2;
+// }
+// const result = numbers.map(doubleIt);
+// console.log(result);
+
+
+
+// const multiplication = n => n*2;
+// const result1 = numbers.map(doubleIt);
+// console.log(result);
+
+// const result2 = numbers.map(n => n*2);
+// console.log(result2);
