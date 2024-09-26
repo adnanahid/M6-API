e4fa7b5caea39b813ff21a205f7c62e24a2a689d
adnanahid <sikuahamed@gmail.com>
2024-09-26 21:24:34 +0600

    done2

--- Module 31/chain.js
+++ Module 31/chain.js
@@ -1,12 +1,25 @@
 // const data = [{id: 1, name:'abul', address: 'dhaka'}]
 // console.log(data[0].address);
 
-const products = {
-    count: 5000,
-    data: [
-        {id: 1, name: 'lenovo laptop', price: 65000},
-        {id: 2, name:'Asus zenbook', price: 70000}
-    ]
-}
+// const products = {
+//     count: 5000,
+//     data: [
+//         {id: 1, name: 'lenovo laptop', price: 65000},
+//         {id: 2, name:'Asus zenbook', price: 70000}
+//     ]
+// }
+// console.log(products.data[1].name);
 
-console.log(products.data[1].name);
+const user = {
+    id: 5022,
+    name: 'Adnan_Nahid',
+    address: {
+        place: {
+            first: 'New Market',
+            second: '7 number',
+            third: 'Batiyaghata'
+        }
+    },
+    city: 'khulna'
+}
+console.log(user.address.street?.first);
