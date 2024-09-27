5c4577c04d401f52083cc9f29b95aee5d4bd48e4
adnanahid <sikuahamed@gmail.com>
2024-09-27 10:19:46 +0600

    task1sone

--- Module 31/map.js
+++ Module 31/map.js
@@ -1,8 +1,8 @@
-const numbers = [4, 5, 6, 7, 8, 9];
-const find = numbers.find(n => n>9)
-console.log(find);
-const filter = numbers.filter(n => n>9)
-console.log(filter);
+// const numbers = [4, 5, 6, 7, 8, 9];
+// const find = numbers.find(n => n>9)
+// console.log(find);
+// const filter = numbers.filter(n => n>9)
+// console.log(filter);
 // const doubled = [];
 // for (const num of numbers) {
 //   const double = num * 2;
@@ -26,3 +26,22 @@ console.log(filter);
 
 // const result2 = numbers.map(n => n*2);
 // console.log(result2);
+
+
+
+
+// const num = [1,2,3,4,5];
+// let output = num.filter(n => n%2);
+// console.log(output);
+
+const product = {name: 'Laptop', model:'Yoga 3', price:49000, dusk: '512SSD'}
+const {name, model, price} = product;
+console.log(name);
+console.log(model);
+console.log(price);
+
+let [num1, num2, num3, ...restItems] = [1,2,3,4,5,6,7,8,9];
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(restItems);
